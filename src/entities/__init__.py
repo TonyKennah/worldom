edd5d36db815809cc5d8fbd349a251c6c3bc4b79
wonderinglostsoul44 <et_pj84@myyahoo.com>
2025-08-16 22:09:59 +1000

    Create __init__.py

---
+++ src/entities/__init__.py
@@ -0,0 +1,12 @@
+from .unit import Unit
+from .effects import EffectManager, SpeedBoost, Slow, Stun, DamageOverTime, FlashPulse
+
+__all__ = [
+    "Unit",
+    "EffectManager",
+    "SpeedBoost",
+    "Slow",
+    "Stun",
+    "DamageOverTime",
+    "FlashPulse",
+]
