b1380cae51d6b2598928531a16c2015bc42c2b58
wonderinglostsoul44 <et_pj84@myyahoo.com>
2025-08-22 13:45:00 +1000

    Update ui_manager.py

--- src/ui/ui_manager.py
+++ src/ui/ui_manager.py
@@ -85,7 +85,7 @@ def toggle_help_overlay(self) -> None:
     # DRAW
     # -------------------------------------------------------------------------
     def draw_ui(self) -> None:
-    """Draws all UI elements, called once per frame."""
+        """Draws all UI elements, called once per frame."""
     # Draw selection box
     if self.game.world_state.selection_box:
         pygame.draw.rect(
