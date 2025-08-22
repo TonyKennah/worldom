8590b5b462119cb17cf6b0188e35cebedcfdf9d2
wonderinglostsoul44 <et_pj84@myyahoo.com>
2025-08-22 11:02:29 +1000

    Delete src/src directory

--- src/src/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Top-level package marker for WorldDom.
-
-Having an __init__ here ensures imports like
-`from src.utils.settings import ...` work consistently on all environments,
-including tools and test runners that don't inject the project root to sys.path.
-"""
-__all__ = ["core", "ui", "entities", "world", "utils", "rendering"]
