8b6da72d11d57273c588167fb0a185e42806f589
wonderinglostsoul44 <et_pj84@myyahoo.com>
2025-08-22 13:31:01 +1000

    Update platform_init.py

--- src/utils/platform_init.py
+++ src/utils/platform_init.py
@@ -5,7 +5,13 @@ import os
 import sys
 import platform
 import pygame
-from typing import Tuple 
+from typing import Tuple, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    import pygame
+
+def some_function(..., flags: int = 0) -> Tuple["pygame.Surface", "pygame.time.Clock"]:
+    ...
 
 def apply_platform_tweaks(*, headless: bool = False) -> None:
     """
